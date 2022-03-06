97df0a47579a64ec074b0949b67f8af4cc833a8f
Marcelo Valentim <vdomarcelo@gmail.com>
2022-03-06 20:40:19 -0300

    adding navbar and board, and initial styles

---
+++ src/Board.js
@@ -0,0 +1,7 @@
+import styles from "./Board.module.css";
+
+function Board() {
+  return <div className={styles.Board}>BOARD</div>;
+}
+
+export default Board;
---
+++ src/NavBar.js
@@ -0,0 +1,7 @@
+import styles from "./NavBar.module.css";
+
+function NavBar() {
+  return <div className={styles.NavBar}>NAV BAR</div>;
+}
+
+export default NavBar;
