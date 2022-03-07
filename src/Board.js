a6d472d827c3b00583864178ae8251daa4da0f80
Marcelo Valentim <vdomarcelo@gmail.com>
2022-03-07 17:59:34 -0300

    adding button to add new lists

--- src/Board.js
+++ src/Board.js
@@ -5,11 +5,18 @@ import styles from "./Board.module.css";
 function Board() {
   const [listList, setListList] = useState(["To Do", "Doing", "Done"]);
 
+  const addNewList = () => {
+    setListList((prev) => prev.concat("New List"));
+  };
+
   return (
     <div className={styles.Board}>
       {listList.map((listName, i) => {
         return <List key={i} listName={listName} />;
       })}
+      <button className={styles.buttonAdd} onClick={addNewList}>
+        + Add Another List
+      </button>
     </div>
   );
 }
