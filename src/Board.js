e7f127b74042d5b253bf7aec2ed622944d7eaf76
Marcelo Valentim <vdomarcelo@gmail.com>
2022-03-06 23:30:46 -0300

    adding example cards

--- src/Board.js
+++ src/Board.js
@@ -1,7 +1,13 @@
+import Card from "./Card";
 import styles from "./Board.module.css";
 
 function Board() {
-  return <div className={styles.Board}>BOARD</div>;
+  return (
+    <div className={styles.Board}>
+      <Card />
+      <Card />
+    </div>
+  );
 }
 
 export default Board;
---
+++ src/Card.js
@@ -0,0 +1,7 @@
+import styles from "./Card.module.css";
+
+function Card() {
+  return <div className={styles.Card}>CARD</div>;
+}
+
+export default Card;
