a362514a38d54e38dddf34b0a208290046c65755
Marcelo Valentim <vdomarcelo@gmail.com>
2022-03-29 15:57:52 -0300

    first get

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = 80;
 
+app.get("/", (req, res) => {
+  console.log("Getting front page");
+});
+
 app.listen(port, () => {
   console.log(`Serving on port ${port}`);
 });
