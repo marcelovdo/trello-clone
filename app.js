e990ba543db91baf80e9b8802d84e8a4a0453c23
Marcelo Valentim <vdomarcelo@gmail.com>
2022-04-02 11:47:54 -0300

    adding json data on memory. adding initial routes.

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ const app = express();
 
 const port = 80;
 
-app.get("/", (req, res) => {
-  console.log("Getting front page");
-});
+let db = {
+  "To Do": [],
+  Doing: [],
+  Done: [],
+};
+
+app.get("/lists", (req, res) => {});
+
+app.post("/lists/new", (req, res) => {});
+
+app.get("/lists/:id/cards", (req, res) => {});
+
+app.post("/lists/:id/cards/new", (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Serving on port ${port}`);
