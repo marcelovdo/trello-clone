c8c9e941532d4936f1c252cfcc01ca3fa23f0ae7
Marcelo Valentim <vdomarcelo@gmail.com>
2022-04-03 13:43:14 -0300

    calling list post and delete API methods. fixing UI to work with the API interface.

--- src/data/ListData.js
+++ src/data/ListData.js
@@ -3,9 +3,23 @@ import { BACKEND_URL } from "./DataConfig";
 export const fetchLists = async () => {
   const response = await fetch(`${BACKEND_URL}/lists`);
   const lists = await response.json();
-  return lists.listNames;
+  return lists.lists;
 };
 
-export const postList = async () => {};
+export const postList = async (listName) => {
+  const fetchOptions = {
+    method: "POST",
+    body: JSON.stringify({ listName: listName }),
+    headers: { "Content-Type": "application/json" },
+  };
+  const response = await fetch(`${BACKEND_URL}/lists/new`, fetchOptions);
+  const resData = await response.json();
+  return resData._id;
+};
 
-export const deleteList = async () => {};
+export const deleteList = async (id) => {
+  const fetchOptions = {
+    method: "DELETE",
+  };
+  await fetch(`${BACKEND_URL}/lists/${id}`, fetchOptions);
+};
--- src/sections/Board.js
+++ src/sections/Board.js
@@ -2,11 +2,11 @@ import { useState, useEffect } from "react";
 import List from "./List";
 import AddListButton from "../buttons/AddListButton";
 import FormAddList from "../forms/FormAddList";
-import { fetchLists } from "../data/ListData";
+import { fetchLists, postList, deleteList } from "../data/ListData";
 import styles from "./Board.module.css";
 
 function Board() {
-  const [listList, setListList] = useState(["To Do", "Doing", "Done"]);
+  const [listList, setListList] = useState([]);
   const [isAddingList, setIsAddingList] = useState(false);
 
   useEffect(() => {
@@ -17,6 +17,11 @@ function Board() {
     fetchListData();
   }, []);
 
+  const asyncPostList = async (listName) => {
+    const id = await postList(listName);
+    return id;
+  };
+
   const beginAdding = () => {
     setIsAddingList(true);
   };
@@ -25,23 +30,30 @@ function Board() {
     setIsAddingList(false);
   };
 
-  const finishAdding = (listName) => {
+  const finishAdding = async (listName) => {
     setIsAddingList(false);
-    setListList((prev) => prev.concat([listName]));
+    const id = await asyncPostList(listName);
+    setListList((prev) => prev.concat([{ name: listName, _id: id }]));
   };
 
   const removeList = (id) => {
-    setListList((prev) => prev.filter((_, i) => i !== id));
+    deleteList(id);
+    setListList((prev) => prev.filter((list) => list._id !== id));
   };
 
   return (
     <div className={styles.Board}>
-      {listList.map((listName, i) => (
-        <List key={i} id={i} listName={listName} onRemove={removeList} />
+      {listList.map((list) => (
+        <List
+          key={list._id}
+          id={list._id}
+          listName={list.name}
+          onRemove={removeList}
+        />
       ))}
       {isAddingList ? (
         <FormAddList
-          onFinishAdding={finishAdding}
+          onFinishAdding={(listName) => finishAdding(listName)}
           onClose={closeAddlListForm}
         />
       ) : (
