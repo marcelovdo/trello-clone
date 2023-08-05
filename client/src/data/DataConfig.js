b5d07da890d4a4bb303495a2110b1680ff1603ac
Marcelo Valentim <vdomarcelo@gmail.com>
2023-08-04 23:07:41 -0300

    fixing port when client is accessing server

--- client/src/data/DataConfig.js
+++ client/src/data/DataConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   BACKEND_URL:
     process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
+      ? "http://localhost:80"
       : "https://desolate-castle-09081.herokuapp.com",
 };
