f6dc131605ca4868a357845a4a7d56bfe76696a9
Marcelo Valentim <vdomarcelo@gmail.com>
2023-08-06 16:59:39 -0300

    converting code to ES Modules format

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const morgan = require("morgan");
-const { v4: uuidv4 } = require("uuid");
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
+import { v4 as uuidv4 } from "uuid";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const accessLogStream = fs.createWriteStream(
   path.join(__dirname, "logs", "access.log"),
