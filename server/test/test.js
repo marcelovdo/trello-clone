37e3633ca45ee311186c9e49b226129d8ef436c7
Marcelo Valentim <vdomarcelo@gmail.com>
2023-08-31 16:12:44 -0300

    adding chai and sinon packages

--- server/test/test.js
+++ server/test/test.js
@@ -1,9 +1,13 @@
-import assert from 'assert';
+import { expect } from 'chai';
 
-describe('Array', function () {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1)
-    })
+describe('Controllers', function() {
+  it('', function () {
+    
   })
 })
+
+//describe('Array', function () {
+//    it('should return -1 when the value is not present', function () {
+//      expect([1, 2, 3].indexOf(4)).to.equal(-1)
+//    })
+//})
